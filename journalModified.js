edd40578b6f646b574c9af4de399695891cf8c0d
shohan494 <shohan12224943@gmail.com>
2017-06-19 03:27:01 +0600

    finalized journalModified.js file

--- journalModified.js
+++ journalModified.js
@@ -91,8 +91,6 @@ var JOURNAL = [
   {"events":["cauliflower","peanuts","brushed teeth","weekend"],"squirrel":false}
 ];
 
-
-
 function phi(table) {
   return (table[3] * table[0] - table[2] * table[1]) /
     Math.sqrt((table[2] + table[3]) *
@@ -101,9 +99,6 @@ function phi(table) {
               (table[0] + table[2]));
 }
 
-//console.log(JOURNAL);
-//console.log("\n");
-
 function hasEvent(event, entry) {
   return entry.events.indexOf(event) != -1;
 }
@@ -111,23 +106,39 @@ function hasEvent(event, entry) {
 function tableFor(event, journal) {
   var table = [0, 0, 0, 0];
   for (var i = 0; i < journal.length; i++) {
-    var entry = journal[i],
-        index = 0;
-    //loop is going through by 'index'
+    var entry = journal[i], index = 0;
     if (hasEvent(event, entry)) index += 1;
     if (entry.squirrel) index += 2;
     table[index] += 1;
-    console.log(index);
-    //console.log(table[index]);
-    console.log(table);
   }
   return table;
 }
 
-console.log("Journal length: ", JOURNAL.length);
-console.log("\n");
-console.log(JOURNAL[0]);
-console.log("\n");
-console.log(tableFor("pizza", JOURNAL));
+function gatherCorrelations(journal) {
+  var phis = {};
+  for (var entry = 0; entry < journal.length; entry++) {
+    var events = journal[entry].events;
+    for (var i = 0; i < events.length; i++) {
+      var event = events[i];
+      if (!(event in phis))
+        phis[event] = phi(tableFor(event, journal));
+    }
+  }
+  return phis;
+}
 
-// → [76, 9, 4, 1]
+var correlations = gatherCorrelations(JOURNAL);
+console.log(correlations.pizza);
+// → 0.068599434
+console.log("\n")
+for (var eventName in correlations)
+  console.log(eventName + ": " + correlations[eventName]);
+  
+for (var i = 0; i < JOURNAL.length; i++) {
+  var entry = JOURNAL[i];
+  if (hasEvent("peanuts", entry) &&
+     !hasEvent("brushed teeth", entry))
+    entry.events.push("peanut teeth");
+}
+console.log(phi(tableFor("peanut teeth", JOURNAL)));
+// → 1
