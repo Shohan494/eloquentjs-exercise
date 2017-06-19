aed5f2555bf9d77eb08f024982b6804a00fdb231
shohan494 <shohan12224943@gmail.com>
2017-06-19 15:05:53 +0600

    high order function as closure example

---
+++ hofClosure.js
@@ -0,0 +1,27 @@
+function greaterThan(n) {
+  return function(m) { return m > n; };
+}
+var greaterThan10 = greaterThan(10);
+console.log(greaterThan10(11));
+// → true
+
+
+function noisy(f) {
+  return function(arg) {
+    console.log("calling with", arg);
+    var val = f(arg);
+    console.log("called with", arg, "- got", val);
+    return val;
+  };
+}
+noisy(Boolean)(0);
+// calling function noisy(Boolean)(0)
+
+//console.log(Boolean(0));
+
+//Output
+// → calling with 0
+// → called with 0 - got false
+
+
+
